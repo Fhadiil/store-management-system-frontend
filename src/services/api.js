55922b42d12fbf3c6eb92489ccad44ad8e91a11c
Fhadiil <fadeelaliyu51@gmail.com>
2026-06-30 20:52:54 +0100

    changed server url

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,20 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_URL =
-  "https://store-management-system-backend-wr2w.onrender.com/store/api";
+const API_URL = "http://127.0.0.1:8000/store/api";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
-
-  getSalesReport: (params) =>
-    axios.get(
-      "https://store-management-system-backend-wr2w.onrender.com/store/api/reports/sales/",
-      { params }
-    ),
-  getInventoryReport: (params) =>
-    axios.get(
-      "https://store-management-system-backend-wr2w.onrender.com/store/api/reports/inventory/",
-      { params }
-    ),
-  exportSalesReport: (params) =>
-    axios.get(
-      "https://store-management-system-backend-wr2w.onrender.com/store/api/reports/sales/export/",
-      {
-        params,
-        responseType: "blob",
-      }
-    ),
 });
+
+api.getSalesReport = (params) => api.get("/reports/sales/", { params });
+
+api.getInventoryReport = (params) => api.get("/reports/inventory/", { params });
+
+api.exportSalesReport = (params) =>
+  api.get("/reports/sales/export/", { params, responseType: "blob" });
 
 export default api;
